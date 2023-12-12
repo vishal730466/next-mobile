2f3007a9bd5e05f59640e1dce52f2cebde8609d9
vishale <vishalingle9876@gmail.comm>
2023-12-12 20:55:34 +0530

    toggle

--- src/app/Nav/page.js
+++ src/app/Nav/page.js
@@ -1,12 +1,14 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import './nav_style.css'
+
 import { useRef, useEffect, useState } from 'react';
 
 export const Nav = () => {
   const divRef = useRef(null);
   const [width, setWidth] = useState(0);
-
+  const [l1, setl1] = useState(false);
 
   useEffect(() => {
     const divElement = divRef.current;
@@ -16,7 +18,20 @@ export const Nav = () => {
       setWidth(elementWidth);
     }
 
+
   }, []);
+
+  function Toggle() {
+    var divs = document.getElementsByClassName("toggle");
+    for (var i = 0; i < divs.length; i++) {
+      if (divs[i].style.display === "none" || divs[i].style.display === "") {
+        divs[i].style.display = "block";
+      } else {
+        divs[i].style.display = "none";
+      }
+    }
+  }
+  
 
   if (width > 500) {
 
@@ -24,7 +39,7 @@ export const Nav = () => {
 
       <div>
 
-        <ul className='nav' >
+        <ul className='nav' > 
           <Link href="/"><li className="myul"> Home</li></Link>
           <Link href="/about"> <li className="myul">About</li></Link>
           <Link href="/Nav"> <li className="myul">Contact</li></Link>
@@ -39,9 +54,13 @@ export const Nav = () => {
 
   else {
     return (
-      <div ref={divRef} style={{ width: "100%", color: "red" }}>
-        Your Element
-        <p>small than 500 : {width}px</p>
+      <div ref={divRef} style={{ width: "100%" }}>
+        <ul className='m_nav' > <div className='toggle'>
+          <Link href="/"><li className=""> Home</li></Link>
+          <Link href="/about"> <li className="">About</li></Link>
+          <Link href="/Nav"> <li className="">Contact</li></Link></div>
+        </ul>
+        <button onClick={Toggle}> click me</button>
       </div>
     )
   }
